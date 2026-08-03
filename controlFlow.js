71a816a893b3f953a05613b041c3cf1c9ead6464
nafiuldev01 <devnafiul@gmail.com>
2026-08-03 21:04:21 +0600

    control flow

---
+++ controlFlow.js
@@ -0,0 +1,63 @@
+// control flow hocche amra kichu condition er upor decide korte pari kon code run hobe r kon code ignore hobe
+
+// <,>,<=,>=,==,===,!=,!==
+
+// if (2 < 3) {
+//   console.log("Executed"); // this code will be executed because the given condition is true
+// }
+
+// if (2 > 3) {
+//   console.log("Executed"); // this code will not run because the given condition is not true
+// }
+
+// if (2 <= 2) {
+//   console.log("Executed"); // the given condition is true so the code will run
+// }
+
+// if (2 >= 2) {
+//   console.log("Executed"); // this code will run the given condition is true
+// }
+
+// if (2 !== 1) {
+//   console.log("Executed"); // this code will run the condition is true
+// }
+
+// == double equals only check the value not the type
+
+// if (2 == "2") {
+//   console.log("Executed"); // this code will run because double equals only check the value not the data types so we should always use === equals for double check
+// }
+
+// if (2 === "2") {
+//   console.log("Executed"); // this condition will not run because the data types are not the same
+// }
+
+// we can also create 2 blocks which will run depending upon condition
+
+// let score = 99;
+// let power = "";
+// if (score > 100) {
+//   power = "Fly";
+//   console.log(`User power: ${power}`);
+// } else {
+//   power = "No Power";
+//   console.log(`User Power: ${power}`);
+// }
+
+// const balance = 1;
+// if (balance) console.log(`You have ${balance}Taka in you account`);
+// else console.log(`You have no balance in your account`);
+
+// const user = true;
+// const debitCard = true;
+// const isLoggedInGoogle = true;
+// const isLoggedInEmail = false;
+
+// if (isLoggedInGoogle || isLoggedInEmail) {
+//   console.log("User logged in");
+//   if (user && debitCard) {
+//     console.log("Can buy courses");
+//   } else {
+//     console.log("Cannot buy courses");
+//   }
+// }
