f863d95ff367026a1db4cc7a60f0416e4c78efe2
nafiuldev01 <devnafiul@gmail.com>
2026-08-04 01:43:48 +0600

    control flow

--- switch.js
+++ switch.js
@@ -5,3 +5,65 @@
 //   default:
 //     break;
 // }
+
+// let month = 13;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+//   case 6:
+//     console.log("June");
+//     break;
+//   case 7:
+//     console.log("July");
+//     break;
+//   case 8:
+//     console.log("August");
+//     break;
+//   case 9:
+//     console.log("September");
+//     break;
+//   case 10:
+//     console.log("October");
+//     break;
+//   case 11:
+//     console.log("November");
+//     break;
+//   case 12:
+//     console.log("December");
+//     break;
+
+//   default:
+//     console.log("None matched");
+//     break;
+// }
+
+let age = 60;
+
+switch (age) {
+  case 18:
+    console.log("Adult");
+    break;
+  case 30:
+    console.log("Mid age");
+    break;
+  case 60:
+    console.log("End times");
+    break;
+  default:
+    console.log("none matched");
+    break;
+}
