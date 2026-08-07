94575c0f27ebc542a478832e2cc4913144cd5c17
nafiuldev01 <devnafiul@gmail.com>
2026-08-08 02:22:57 +0600

    bmi calculator done

--- projects/project-2/index.js
+++ projects/project-2/index.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const target = e.target;
+
   const height = parseInt(e.target.height.value);
   const weight = parseInt(e.target.weight.value);
   const result = document.querySelector(".results");
@@ -14,9 +14,27 @@ form.addEventListener("submit", (e) => {
   if (weight === "" || weight < 0 || isNaN(weight)) {
     return (result.innerHTML = "Please give a valid weight");
   }
-  if (height && weight) {
-    bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    const text = document.createTextNode(`Here is your reslut: ${bmi}`);
-    result.appendChild(text);
+
+  bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  result.innerHTML = `<span>${bmi}</span>`;
+  if (bmi < 18.5) {
+    const span = document.createElement("span");
+    span.innerHTML = "Underweight";
+    return result.appendChild(span);
+  }
+  if (bmi > 18.5 && bmi <= 24.9) {
+    const span = document.createElement("span");
+    span.innerHTML = "Healthy weight";
+    return result.appendChild(span);
+  }
+  if (bmi >= 25.0 && bmi <= 29.9) {
+    const span = document.createElement("span");
+    span.innerHTML = "Overweight";
+    return result.appendChild(span);
+  }
+  if (bmi > 30.0) {
+    const span = document.createElement("span");
+    span.innerHTML = "Obesity";
+    return result.appendChild(span);
   }
 });
