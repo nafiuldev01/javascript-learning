b62ec7db9669e45a023db0e309e525dfe265ab89
nafiuldev01 <devnafiul@gmail.com>
2026-08-07 15:28:04 +0600

    some tweaks in project-1

--- projects/themechanger/themeChanger.js
+++ projects/themechanger/themeChanger.js
@@ -1,8 +1,13 @@
 const buttons = document.querySelectorAll(".button");
+const resetButton = document.querySelector("#reset");
 const body = document.querySelector("body");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     body.style.backgroundColor = e.target.id;
   });
+});
+
+resetButton.addEventListener("click", (e) => {
+  body.style.backgroundColor = "white";
 });
